11a9a38c66a32114ed9463f26616d17db6ba4ee1
k0ala <k0ala.gephi@augrime.net>
2015-07-08 16:39:19 +0200

    add chapter 5 exercises

---
+++ exercises/src/main/scala/fpinscala/Chap5.scala
@@ -0,0 +1,185 @@
+package fpinscala
+
+import fpinscala.laziness._
+
+
+object Chap5 extends App {
+
+
+  /** Ex 5.1
+    *
+    * convert a Stream to a List, which will force its evaluation
+    */
+  assert(Stream.empty[Double].toList == List.empty[Double])
+  assert(Stream(1,2,3).toList == List(1,2,3))
+
+
+  /** Ex 5.2
+    *
+    * Write the function take(n) for returning the first n elements of a
+    * Stream, and drop(n) for skipping the first n elements of a Stream.
+    */
+  assert(Stream(1,2,3,4,5).take(3).toList == List(1,2,3))
+  assert(Stream(1).take(4).toList == List(1))
+  assert(Stream(1,2,3,4,5).drop(3).toList == List(4,5))
+  assert(Stream(1).drop(4).toList == List.empty)
+
+
+  /** Ex 5.3
+    *
+    * Write the function takeWhile for returning all starting elements of a
+    * Stream that match the given predicate.
+    */
+  assert(Stream(1,2,3,4,5).takeWhile(_ < 4).toList == List(1,2,3))
+
+
+  /** Ex 5.4
+    *
+    * checks that all elements in the Stream match a given predicate.
+    * Your implementation should terminate the traversal as soon as it
+    * encounters a nonmatching value.
+    */
+  assert(Stream(1,2,3,4).forAll(_ < 5) == true)
+  assert(Stream.empty[Int].forAll(_ < 5) == true)
+  assert(Stream(1,2,3,4,5).forAll(_ < 5) == false)
+
+
+  /** Ex 5.5
+    *
+    * foldRight in terms of takeWhile.
+    */
+  assert(Stream(1,2,3,4,5).takeWhileFR(_ < 4).toList == List(1,2,3))
+
+
+  /** Ex 5.6
+    *
+    * headOption using foldRight.
+    */
+  assert(Stream(1).headOption == Some(1))
+  assert(Stream(1,2,3).headOption == Some(1))
+  assert(Stream.empty[Double].headOption == (None: Option[Double]))
+
+
+  /** Ex 5.7
+    *
+    * map, filter, append, and flatMap using foldRight. The append method
+    * should be non-strict in its argument.
+    */
+  // map:
+  assert(Stream(1,2,3).mapFR(_ * 2).toList == List(2,4,6))
+  assert(Stream.empty[Double].mapFR(_ * 2.0).toList == List.empty)
+  // filter:
+  assert(Stream(1,2,3,4).filterFR(_ % 2 == 0).toList == List(2,4))
+  assert(Stream.empty[Double].filterFR(_ % 2 == 0).toList == List.empty)
+  // append:
+  assert(Stream.empty[Double].append(Stream(1.0,2.0)).toList == List(1.0,2.0))
+  assert(Stream(1.0,2.0).append(Stream.empty[Double]).toList == List(1.0,2.0))
+  assert(Stream(1,2).append(Stream(3,4)).toList == List(1,2,3,4))
+  // flatMap:
+  assert(Stream(1,3).flatMap(x => Stream(x + 1, x + 2)).toList ==
+    Stream(2,3,4,5).toList)
+  assert(Stream.empty[Int].flatMap(x => Stream(x + 1, x + 2)).toList ==
+    List.empty)
+  assert(Stream(1,3).flatMap(x => Stream.empty[Double]).toList == List.empty)
+
+
+  /** Ex 5.8
+    *
+    * Generalize ones slightly to the function constant, which returns an
+    * infinite Stream of a given value.
+    */
+  assert(Stream.const(3).take(5).toList == List(3,3,3,3,3))
+  assert(Stream.const(3).take(0).toList == List())
+
+
+  /** Ex 5.9
+    *
+    * generates an infinite stream of integers, starting from n.
+    */
+  assert(Stream.from(7).take(3).toList == List(7,8,9))
+
+
+  /** Ex 5.10
+    *
+    * generates the infinite stream of Fibonacci numbers:
+    * 0, 1, 1, 2, 3, 5, 8, and so on.
+    */
+  assert(Stream.fibs(0,1).take(7).toList == List(0, 1, 1, 2, 3, 5, 8))
+
+  /** Ex 5.11
+    *
+    * takes an initial state, and a function for producing both the next
+    * state and the next value in the generated stream.
+    */
+  assert(Stream.unfold(1){ x =>
+    if (x<5) Some("*"*x, x*2) else None}.toList == List("*", "**", "****"))
+
+
+  /** Ex 5.12
+    *
+    * fibs, from, constant, and ones in terms of unfold.
+    */
+  assert(Stream.onesU.take(3).toList == List(1,1,1))
+  assert(Stream.constU("whee").take(3).toList == List("whee","whee","whee"))
+  assert(Stream.fromU(5).take(3).toList == List(5,6,7))
+  assert(Stream.fibsU(0,1).take(7).toList == List(0, 1, 1, 2, 3, 5, 8))
+
+
+  /** Ex 5.13
+    *
+    * map, take, takeWhile, zipWith  and zipAll in terms of unfold.
+    * The zipAll function should continue the traversal as long as
+    * either stream has more elements—it uses Option to indicate whether
+    * each stream has been exhausted.
+    */
+  assert(Stream.fibs(0,1).mapU(_ * 2).take(5).toList == List(0,2,2,4,6))
+  assert(Stream.fibs(0,1).takeU(7).toList == List(0, 1, 1, 2, 3, 5, 8))
+  assert(Stream(1,2,3,4,5).takeWhileU(_ < 4).toList == List(1,2,3))
+  assert(Stream(1,2,3).zipWithU(Stream(4,5)).toList == List((1,4),(2,5)))
+  assert(Stream(1,2).zipWithU(Stream(4,5,6)).toList == List((1,4),(2,5)))
+  assert(Stream(1,2).zipAllU(Stream(3,4)).toList ==
+    List((Some(1),Some(3)),(Some(2),Some(4))))
+  assert(Stream(1,2).zipAllU(Stream(3)).toList ==
+    List((Some(1),Some(3)),(Some(2),None)))
+  assert(Stream(1).zipAllU(Stream(3,4,5)).toList ==
+    List((Some(1),Some(3)),(None, Some(4)),(None, Some(5))))
+
+
+  /** (Ex "5.13b")
+    *
+    * hasSub determines whether a stream has a given subsequence.
+    */
+  assert(Stream.empty.hasSub(List(1,2,3)) == false)
+  assert(Stream(1,2,3,4).hasSub(List(1,2,3)) == true)
+  assert(Stream(1,2,3,4).hasSub(List(1,2,3,4)) == true)
+  assert(Stream(1,2,3,4).hasSub(List(2,3,4)) == true)
+  assert(Stream(1,2,1,2,3).hasSub(List(1,2,3)) == true)
+
+
+  /** Ex 5.14
+    *
+    * check if one Stream is a prefix of another.
+    */
+  assert(Stream(1,2,3) startsWith Stream.empty)
+  assert(Stream(1,2,3) startsWith Stream(1,2))
+  assert(Stream(1,2,3).startsWith(Stream(2,3)) == false)
+
+
+  /** Ex 5.15
+    *
+    * tails in terms of unfold; For a given Stream, tails returns the Stream
+    * of suffixes of the input sequence, including the original Stream.
+    */
+  assert(Stream(1,2,3).tails.toList.map(_.toList) ==
+    List(List(1,2,3), List(2,3), List(3), List()))
+  assert(Stream.empty[Int].tails.toList == List(Empty))
+
+  /** Ex 5.16
+    *
+    * like a foldRight that returns a stream of the intermediate results.
+    */
+  Stream(1,2,3).scanRight(0)(_ + _)
+  assert(Stream(1,2,3).scanRight(0)(_ + _).toList == List(6,5,3,0))
+
+
+}
--- exercises/src/main/scala/fpinscala/laziness/Stream.scala
+++ exercises/src/main/scala/fpinscala/laziness/Stream.scala
@@ -3,13 +3,20 @@ package fpinscala.laziness
 import Stream._
 trait Stream[+A] {
 
+  def toList: List[A] = {
+    this match {
+      case Cons(h,t) => h() :: t().toList
+      case _ => Nil
+    }
+  }
+
   def foldRight[B](z: => B)(f: (A, => B) => B): B = // The arrow `=>` in front of the argument type `B` means that the function `f` takes its second argument by name and may choose not to evaluate it.
     this match {
       case Cons(h,t) => f(h(), t().foldRight(z)(f)) // If `f` doesn't evaluate its second argument, the recursion never occurs.
       case _ => z
     }
 
-  def exists(p: A => Boolean): Boolean = 
+  def exists(p: A => Boolean): Boolean =
     foldRight(false)((a, b) => p(a) || b) // Here `b` is the unevaluated recursive step that folds the tail of the stream. If `p(a)` returns `true`, `b` will never be evaluated and the computation terminates early.
 
   @annotation.tailrec
@@ -17,20 +24,108 @@ trait Stream[+A] {
     case Empty => None
     case Cons(h, t) => if (f(h())) Some(h()) else t().find(f)
   }
-  def take(n: Int): Stream[A] = sys.error("todo")
 
-  def drop(n: Int): Stream[A] = sys.error("todo")
+  def take(n: Int): Stream[A] = (this, n) match {
+    case (_, 0) => Empty
+    case (Empty, _) => Empty
+    case (Cons(h, t), _) => Cons(h, () => t().take(n - 1))
+  }
 
-  def takeWhile(p: A => Boolean): Stream[A] = sys.error("todo")
+  def drop(n: Int): Stream[A] = (this, n) match {
+    case (_, 0) => this
+    case (Empty, _) => this
+    case (Cons(h,t), _) => t().drop(n-1)
+  }
 
-  def forAll(p: A => Boolean): Boolean = sys.error("todo")
+  def takeWhile(p: A => Boolean): Stream[A] = this match {
+    case Cons(h,t) if p(h()) => Cons(h, () => t().takeWhile(p))
+    case _ => empty
+  }
 
-  def headOption: Option[A] = sys.error("todo")
+  def takeWhileFR(p: A => Boolean): Stream[A] = {
+    foldRight(Stream.empty[A])(
+      (a,acc) => if (p(a)) cons(a, acc) else acc)
+  }
+
+  def forAll(p: A => Boolean): Boolean = this match {
+    case Empty => true
+    case Cons(h,_) if (p(h()) == false) => false
+    case Cons(_,t) => t().forAll(p)
+  }
+
+  def headOption: Option[A] = {
+    foldRight(None: Option[A])((a, acc) => Some(a) orElse acc)
+  }
 
   // 5.7 map, filter, append, flatmap using foldRight. Part of the exercise is
   // writing your own function signatures.
+  def mapFR[B](f: A => B): Stream[B] =
+    foldRight(Stream.empty[B])((a, acc) => cons(f(a), acc))
 
-  def startsWith[B](s: Stream[B]): Boolean = sys.error("todo")
+  def filterFR(f: A => Boolean): Stream[A] =
+    foldRight(Stream.empty[A])((a, acc) => if (f(a)) cons(a, acc) else acc)
+
+  def append[B >: A](bs: => Stream[B]): Stream[B] =
+    foldRight(bs: Stream[B])((a, acc) => cons(a, acc))
+
+  def flatMap[B](f: A => Stream[B]): Stream[B] =
+    foldRight(Stream.empty[B])((a, acc) => f(a).append(acc))
+
+  def startsWith[B](s: Stream[B]): Boolean =
+    this.zipWithU(s).forAll{ case (a,b) => a == b }
+
+
+  def mapU[B](f: A => B): Stream[B] = unfold(this: Stream[A]){
+    case Cons(a, s) => Some((f(a()), s()))
+    case _ => None
+  }
+
+  def takeU(n: Int): Stream[A] = unfold((n,this)){
+    case (i, Cons(a, s)) => if (i <= 0) None else Some((a(), (i-1, s()))) }
+
+  def takeWhileU(p: A => Boolean): Stream[A] =
+    unfold(this){
+      case Cons(a, s) if p(a()) => Some((a(), s()))
+      case _ => None
+    }
+
+  def zipWithU[B](bs: Stream[B]): Stream[(A,B)] =
+    unfold((this, bs)){
+      case (Cons(a, aa), Cons(b, bb)) => Some((a(),b()), (aa(),bb()))
+      case _ => None
+    }
+
+  def zipAllU[B](bs: Stream[B]): Stream[(Option[A],Option[B])] =
+    unfold((this, bs)) {
+      case (Cons(a, aa), Cons(b, bb)) => Some((Some(a()),Some(b())), (aa(),bb()))
+      case (Cons(a, aa), _) => Some(((Some(a()), None), (aa(), Stream.empty)))
+      case (_, Cons(b, bb)) => Some(((None, Some(b())), (Stream.empty, bb())))
+      case _ => None
+    }
+
+  def hasSub[B >: A](sub: List[B]): Boolean = {
+    (this, sub) match {
+      case (Cons(a, as), s :: ss) =>
+        if (a()==s) (as().hasSub(ss) || as().hasSub(sub))
+        else as().hasSub(sub)
+      case (_, List()) => true
+      case _ => false
+    }
+  }
+
+  def tails: Stream[Stream[A]] =
+    unfold(Some(this): Option[Stream[A]]){
+      case Some(Cons(a, as)) => Some((Cons(a, as), Some(as())))
+      case Some(_) => Some((Stream.empty, None))
+      case None => None
+    }
+
+  def scanRightT[B](z: B)(f: (A, => B) => B): Stream[B] =
+    this.tails.mapU(_.foldRight(z)(f))
+
+  def scanRight[B](z: B)(f: (A, => B) => B): Stream[B] =
+    foldRight(Stream(z)){ case (a, Cons(b, bs)) =>
+      cons(f(a,b()), Cons(b, bs)) }
 }
 case object Empty extends Stream[Nothing]
 case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
@@ -45,11 +140,27 @@ object Stream {
   def empty[A]: Stream[A] = Empty
 
   def apply[A](as: A*): Stream[A] =
-    if (as.isEmpty) empty 
+    if (as.isEmpty) empty
     else cons(as.head, apply(as.tail: _*))
 
   val ones: Stream[Int] = Stream.cons(1, ones)
-  def from(n: Int): Stream[Int] = sys.error("todo")
 
-  def unfold[A, S](z: S)(f: S => Option[(A, S)]): Stream[A] = sys.error("todo")
+  def const[A](c: A): Stream[A] = Stream.cons(c, const(c))
+
+  def from(n: Int): Stream[Int] = Stream.cons(n, from(n+1))
+
+  def fibs(z: Int, o: Int): Stream[Int] = Stream.cons(z, fibs(o, z+o))
+
+  def unfold[A, S](z: S)(f: S => Option[(A, S)]): Stream[A] =
+    f(z) match {
+      case Some((a,s)) => Stream.cons(a, unfold(s)(f))
+      case None => Stream.empty
+    }
+
+  def fibsU(zero: Int, one: Int) = Stream.unfold((zero,one)){
+    case (z, o) => Some(z, (o, z+o)) }
+  def onesU = Stream.unfold("same"){ s => Some(1, "same") }
+  def fromU(n: Int) = Stream.unfold(n){ s => Some(s, s+1) }
+  def constU[B](a: B) = Stream.unfold(a){ s => Some(s,s) }
+
 }
